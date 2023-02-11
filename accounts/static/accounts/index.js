c9191689d069c6f16142d05824b49bf21499436e
superman <goodnesskolapo@gmail.com>
2023-02-11 15:44:02 +0100

    omo

---
+++ accounts/static/accounts/index.js
@@ -0,0 +1,8 @@
+// $(document).scroll(function() {
+//     var scrollPos = $(document).scrollTop();
+//     if (scrollPos > 0) {
+//       $(".navtexts a").addClass("start-scroll");
+//     } else {
+//       $(".navtexts a").removeClass("start-scroll");
+//     }
+//   });
