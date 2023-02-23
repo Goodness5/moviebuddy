6461995fed67fe35479ff4614c1ba1bd464231a6
superman <goodnesskolapo@gmail.com>
2023-02-24 00:40:57 +0100

    starts this again

--- accounts/static/accounts/index.js
+++ accounts/static/accounts/index.js
@@ -6,3 +6,12 @@ $(document).scroll(function() {
       $(".navtexts a").removeClass("start-scroll");
     }
   });
+
+  // $(document).hover(function() {
+  //   var scrollPos = $(document).scrollTop();
+  //   if (scrollPos > 0) {
+  //     $(".navtexts a").addClass("start-scroll");
+  //   } else {
+  //     $(".navtexts a").removeClass("start-scroll");
+  //   }
+  // });
