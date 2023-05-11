f0d0cdf06e8aee1b3cd587c4693d2b3defb727e8
Ginika Chinonso <ginika.chinonso.francis@gmail.com>
2023-05-11 13:00:49 +0100

    chore: Movies by genre

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import RecommendMovies, MovieList, index
+from .views import RecommendMovies, MovieList, MovieListByGenre, index
 
 urlpatterns = [
     path('movielist/', MovieList.as_view(), name='movie-list'),
+    path('movielist/<str:genre>', MovieListByGenre.as_view(), name='movie-list-genre'),
 ]
