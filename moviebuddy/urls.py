2744163ef664bff1f0ecb5cd5cd28adbd5289657
Ginika Chinonso <ginika.chinonso.francis@gmail.com>
2023-05-11 16:14:46 +0100

    feat: Movies by genre

--- moviebuddy/urls.py
+++ moviebuddy/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path('movies/', include('movies.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
     path('movielist/', views.movielist, name='movie-list'),
-    path('movielist/<str:genre>', views.movielist, name='movie-list-genre'),
+    path('movielist/<str:genre>', views.movielistbygenre, name='movie-list-genre'),
   
 ] 
